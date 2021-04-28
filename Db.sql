343abe0c2e2936d43ee8e0858f8b853e009a4f87
Arthur <arthur.lambotte51@gmail.com>
2021-04-28 10:57:56 +0200

    peuplement

--- Db.sql
+++ Db.sql
@@ -69,20 +69,25 @@ CREATE TABLE Echange(
 START TRANSACTION;
 
 INSERT INTO Personne VALUES(null, "Admin", "Admin", "0000000000", "8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918", 'admin');
-INSERT INTO Personne VALUES(null, "Guillaume", "SOL", "0611223344", "274c8009224625d893a48ad78481d1130dc31b32c8500b335bfcf73168f6fa54", "guillaume");
-INSERT INTO Personne VALUES(null, "Arthur", "LAMBOTTE", "0666666666", "befa156f0283eb0062beb9b86e16a413e1cf8c5135e5518d5c4fa321ce0c7b6b", "arthur");
+
 
 INSERT INTO Administrateur VALUES(1);
 
+CREATE TRIGGER `Ajout_user` AFTER INSERT ON `personne`
+ FOR EACH ROW INSERT INTO Cave Values(null, "Maison Principale", NEW.Id_Personne);
 
-INSERT INTO Utilisateur VALUES(2);
-INSERT INTO Utilisateur VALUES(3);
+CREATE TRIGGER `Ajout_utilisateur` AFTER INSERT ON `personne`
+ FOR EACH ROW INSERT INTO Utilisateur Values(NEW.Id_Personne);
+
+INSERT INTO Personne VALUES(null, "Guillaume", "SOL", "0611223344", "274c8009224625d893a48ad78481d1130dc31b32c8500b335bfcf73168f6fa54", "guillaume");
+INSERT INTO Personne VALUES(null, "Arthur", "LAMBOTTE", "0666666666", "befa156f0283eb0062beb9b86e16a413e1cf8c5135e5518d5c4fa321ce0c7b6b", "arthur");
+INSERT INTO Personne VALUES(null, "Tim", "HAGINE", "0653232523", "aac09a648fc382b6f78897595486e691d00de9dfc742f3ba1930464b56eecda6", "Tim");
+INSERT INTO Personne VALUES(null, "Jean", "REGISTRE", "0612142523", "56ec4fee9f20de343a99f2caa8793dadb6c8fbeebeed0f124e397d660112245c", "Jean");
+INSERT INTO Personne VALUES(null, "Guy", "TAR", "0612142523", "55fb96bf33f19ca574d9616fd4bdeb4ed4840a2120aa63824e436bb35ca3176f", "Guy");
 
 
-INSERT INTO Cave Values(null, "Maison Principale", 2);
 INSERT INTO Cave Values(null, "Maison Secondaire", 2);
-INSERT INTO Cave Values(null, "Maison Principale", 3);
-
+INSERT INTO Cave Values(null, "Maison de vacances", 4);
 
 
 INSERT INTO Vin(Id_Vin,Nom,Type,Notation,Echangeable,Année,Quantité,Id_Cave)  VALUES(null, 'Château Mouton Rothschild Pauillac', 'Bordeaux Rouge amer', 'très bon, assez amer', FALSE, 2002, 5, 1);
@@ -92,15 +97,15 @@ INSERT INTO Vin(Id_Vin,Nom,Type,Notation,Echangeable,Année,Quantité,Id_Cave)  
 INSERT INTO Vin(Id_Vin,Nom,Type,Notation,Echangeable,Année,Quantité,Id_Cave)  VALUES(null, 'Château Haut-Brion Pessac-Léognan', 'Bordeaux Rouge amer', 'bonne odeur mais bouchoné', FALSE, 2005, 6, 1);
 INSERT INTO Vin(Id_Vin,Nom,Type,Notation,Echangeable,Année,Quantité,Id_Cave)  VALUES(null, 'Beauséjour (Duffau Lagarosse), Saint Emilion', 'Bordeaux Rouge', '18/20', FALSE, 2002, 2, 3);
 INSERT INTO Vin(Id_Vin,Nom,Type,Notation,Echangeable,Année,Quantité,Id_Cave)  VALUES(null, 'Bastor la Montagne, Sauternes', 'Bordeaux Blanc', '16/20', TRUE, 2012, 8, 3);
+INSERT INTO Vin(Id_Vin,Nom,Type,Notation,Echangeable,Année,Quantité,Id_Cave)  VALUES(null, 'Bastor la Montagne, Sauternes', 'Bordeaux Blanc', 'bon vin', FALSE, 2020, 7, 5);
 
-
-
-CREATE TRIGGER `Ajout_user` AFTER INSERT ON `personne`
- FOR EACH ROW INSERT INTO Cave Values(null, "Maison Principale", NEW.Id_Personne);
-
-CREATE TRIGGER `Ajout_utilisateur` AFTER INSERT ON `personne`
- FOR EACH ROW INSERT INTO Utilisateur Values(NEW.Id_Personne);
-
+INSERT INTO Vin(Id_Vin,Nom,Type,Notation,Echangeable,Année,Quantité,Id_Cave)  VALUES(null, 'Beaulon', 'Pineau des Charentes', 'Bon vin, 15/20', TRUE, 2016, 2, 5);
+INSERT INTO Vin(Id_Vin,Nom,Type,Notation,Echangeable,Année,Quantité,Id_Cave)  VALUES(null, 'Domaine de la Renardiere', 'Arbois', 'Pas goûté', FALSE, 2018, 6, 5);
+INSERT INTO Vin(Id_Vin,Nom,Type,Notation,Echangeable,Année,Quantité,Id_Cave)  VALUES(null, 'winzenberg gewurztraminer', 'Alsace', '18/20', TRUE, 2017, 6, 6);
+INSERT INTO Vin(Id_Vin,Nom,Type,Notation,Echangeable,Année,Quantité,Id_Cave)  VALUES(null, 'winzenberg grains nobles muscat', 'Alsace', 'bon 15/20', FALSE, 2017, 3, 6);
+INSERT INTO Vin(Id_Vin,Nom,Type,Notation,Echangeable,Année,Quantité,Id_Cave)  VALUES(null, 'winzenberg grains nobles muscat', 'Alsace', 'moyen', FALSE, 2015, 6, 2);
+INSERT INTO Vin(Id_Vin,Nom,Type,Notation,Echangeable,Année,Quantité,Id_Cave)  VALUES(null, 'winzenberg grains nobles muscat', 'Alsace', 'bon vin', TRUE, 2017, 2, 5);
+INSERT INTO Vin(Id_Vin,Nom,Type,Notation,Echangeable,Année,Quantité,Id_Cave)  VALUES(null, 'Bastor la Montagne, Sauternes', 'Bordeaux Blanc', 'bon vin', TRUE, 2012, 7, 2);
 
 INSERT INTO echange(Id_Echange, accept, Id_Vin_Recepteur, Id_Vin_Emmetteur, Id_Emmetteur, Id_Recepteur, Date_demande, reponse) VALUES (null,False, 1,7,3,1,NOW(), FALSE);
 INSERT INTO echange(Id_Echange, accept, Id_Vin_Recepteur, Id_Vin_Emmetteur, Id_Emmetteur, Id_Recepteur, Date_demande, reponse) VALUES (null,False, 7,4,2,3,NOW(), FALSE);
